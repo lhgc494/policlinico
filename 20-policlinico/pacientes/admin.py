d7362a7173ba4332db49f1fe7759fe215113842b
lhgc494 <lhgc494@gmail.com>
2026-07-16 17:18:19 -0500

    fix: eliminar archivos basura restantes

--- 20-policlinico/pacientes/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Paciente
-
-@admin.register(Paciente)
-class PacienteAdmin(admin.ModelAdmin):
-    list_display = ('dni', 'apellidos', 'nombres', 'telefono')
-    search_fields = ('dni', 'nombres', 'apellidos')
-
